5bc3261ce6702c9d03d318d2acdf3ebc11026e63
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-10-15 18:53:53 +0900

    docs: add an example

--- examples/main_test.go
+++ examples/main_test.go
@@ -17,6 +17,10 @@ func TestBuildflow(t *testing.T) { //nolint:funlen
 			file:  "hello_world.yaml",
 		},
 		{
+			title: "read command from a file (task.command_file)",
+			file:  "command_file.yaml",
+		},
+		{
 			title: "run tasks in parallel",
 			file:  "parallel.yaml",
 		},
